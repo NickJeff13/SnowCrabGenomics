c68f1af29cc934bfb32e47044037507fcdcf9d0a
NickJeff13 <Nick.w.jeffery3@gmail.com>
2025-03-27 21:08:51 -0300

    Deleting old files and made new map

--- code/R/Map_script.R
+++ code/R/Map_script.R
@@ -1,4 +1,7 @@
-## Load Libraries --------------
+#Map of Atlantic Canada with crab sampling locations included for Figure 1 of manuscript
+
+# Load libraries ----------------------------------------------------------
+
 library(rgeos)
 library(raster)
 library(ggmap)
@@ -13,8 +16,62 @@ library(gplots) # for colour range
 library(rworldmap)
 library(maptools)
 library(lattice)
+library(ggplot2)
+library(sf)
+library(tidyr)
+library(dplyr)
+library(rnaturalearth)
 
-setwd("R:/Science/CESD/HES_MPAGroup/Projects/Snow Crab Genetics/")
+#map projections ----
+latlong <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+CanProj <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=63.390675 +lon_0=-91.86666666666666 +x_0=6200000 +y_0=3000000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+
+
+#Crab fishing areas ----
+cfas <- read_sf("data/shapefiles/dfo_east_snow_crab_areas_erase.shp")%>%
+  st_transform(latlong)
+
+#sample coordinates ----
+crab_coords <- read.csv("data/Pop_Coords2025.csv")%>%
+  st_as_sf(coords=c("Long","Lat"),crs=latlong)%>%
+  st_transform(latlong)
+
+#Basemap ----
+basemap <- rbind(ne_states(country = "Canada",returnclass = "sf")%>%
+                   dplyr::select(name_en,geometry)%>%
+                   st_as_sf()%>%
+                   st_union()%>%
+                   st_transform(latlong)%>%
+                   st_as_sf()%>%
+                   mutate(country="Canada"),
+                 ne_states(country = "United States of America",returnclass = "sf")%>%
+                   dplyr::select(name_en,geometry)%>%
+                   st_as_sf()%>%
+                   st_union()%>%
+                   st_transform(latlong)%>%
+                   st_as_sf()%>%
+                   mutate(country="USA"))
+
+### set plot extent
+plot_extent <- crab_coords%>%
+  st_buffer(100*2000)%>%
+  st_bbox()
+
+
+# Plot the map with ggplot2 -----------------------------------------------
+
+crab_map <- ggplot()+
+  geom_sf(data=basemap)+
+  #geom_sf(data=cfas,fill=NA)+
+  geom_sf(data=crab_coords, aes(fill=Region),colour= "black",shape=21, size=4)+
+  #geom_sf_label(data=crab_coords, aes(label = SampleSite))+
+  coord_sf(expand=0,xlim=plot_extent[c(1,3)],ylim=plot_extent[c(2,4)])+
+  theme_bw();crab_map
+
+ggsave(filename = "Figure1_CrabMap.png",plot = crab_map, device = "png", path = "figures/", width = 10, height = 8, dpi = 320, units = "in")
+
+#OR use this older way with marmap and some base plotting
+
 ##Snow Crab Locations Map#####
 source("PlotBathy2.R")
 
