77a40df7e7e99b21cbf400488373385429cebdc9
NickJeff13 <nick.w.jeffery3@gmail.com>
2025-04-25 14:52:54 -0300

    admixture plot for Q3

--- code/R/lcwgs/Plot_Admixture.R
+++ code/R/lcwgs/Plot_Admixture.R
@@ -1,12 +1,13 @@
 library(dplyr)
+library(tidyverse)
 library(ggplot2)
 
 #read in .Q file  and combine results with .fam file
-tbl=read.table("/mnt/sdb/SnowCrab_LCWGS/MAF_Filtered_Plink/snowcrab.maffiltered.2.Q")
+tbl=read.table("/mnt/sdb/SnowCrab_LCWGS/Admixture_Output/snowcrab.maffiltered.3.Q")
 
 #read .fam file
 
-fam=read.table("data/admixture/exon.recode.fam")
+fam=read.table("data/admixture/snowcrab.maffiltered.fam")
 fam$V1 <- gsub(".*i5.","",fam$V1)
 fam$V1 <- gsub(".realigned.bam","",fam$V1)
 fam$V2 <- gsub(".*i5.","",fam$V2)
@@ -19,11 +20,32 @@ colnames(results_q2)=c("Pop", "ID", "q1","q2","q3") #"q4","q5","q6","q7","q8","q
 #Reorder individuals by Pop
 results_q2[order(results_q2$Pop),]
 
+#reshape to make it easier for ggplot
+
+results_long <- results_q2 %>%
+        pivot_longer(cols=c(q1, q2, q3), names_to = "membership", values_to = "value")
+
+results_long$ID <- factor(results_long$ID, levels=unique(results_q2$ID))
 #plot results quickly
 
-ggplot(results_q2, aes(x=Individual))+
+ggplot(results_long, aes(x=ID, y=value, fill=membership))+
+        geom_bar(stat="identity") +
+        theme_bw()+
+        theme(axis.text.x = element_text(angle=90, hjust=1))
         
+# too many x-axis labels so let's thin them out a bit
+breaks <- levels(results_long$ID)[seq(1, length(levels(results_long$ID)), by = 10)]
+        
+ggplot(results_long, aes(x = ID, y = value, fill = membership)) +
+        geom_bar(stat = "identity") +
+        scale_x_discrete(breaks = breaks) +
+        theme_minimal() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+        labs(x = "", y = "Proportion", fill = "Ancestry")
 
+ggsave(filename = "MAF_Filtered_Admixture_Q3.png", plot = last_plot(), device = "png", path = "figures/", width = 10, height=8, dpi = 300)
+
+#old
 barplot(t(results_q2[order(results_q2$Pop),3:4]), 
         col=c("firebrick","blue","forestgreen", "gold", "turquoise", "grey","limegreen", "aliceblue", "orange","purple"),
         xlab="Individual #", ylab="Ancestry", border=NA, 
