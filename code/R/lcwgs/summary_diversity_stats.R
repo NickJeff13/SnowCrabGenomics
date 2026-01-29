f84a7f988904cb0f2fa15359b81dde6e71ea4f8f
NickJeff13 <nick.w.jeffery3@gmail.com>
2026-01-29 16:15:36 -0400

    updating crab ms figures

--- code/R/lcwgs/summary_diversity_stats.R
+++ code/R/lcwgs/summary_diversity_stats.R
@@ -8,14 +8,15 @@ library(patchwork)
 crab.het <- read.table("data/Exon_SNPs_InbreedingStats.csv", header = T, sep="\t") %>% glimpse()
 
 crab.het <- crab.het %>%
-  mutate(He=(N_SITES-E.HOM.)/N_SITES, Ho=(N_SITES-O.HOM.)/N_SITES)
+  mutate(He=(N_SITES-E.HOM.)/N_SITES, Ho=(N_SITES-O.HOM.)/N_SITES, POP=gsub("Baie des Chal","Chaleurs",POP))
 
 #plot Fis
 p1 <- ggplot()+ 
   geom_boxplot(data = crab.het, aes(x=POP, y=F, fill=POP),colour="black",  alpha=0.7)+
   theme_bw()+
-  guides(fill=guide_legend(nrow=3,byrow=T))+
-  theme(axis.text.x=element_text(angle=45, vjust=0.8, hjust=1), legend.position = "bottom");p1
+  #guides(fill=guide_legend(nrow=3,byrow=T))+
+  theme(axis.text.x=element_text(angle=45, vjust=0.8, hjust=1), 
+        legend.position = "bottom");p1
 
 #Plot He/Ho
 p2 <- ggplot()+ 
@@ -23,20 +24,25 @@ p2 <- ggplot()+
   geom_boxplot(data=crab.het, aes(x=POP, y=He), colour="black")+
   theme_bw()+
   xlab(label="Population")+
-  guides(fill=guide_legend(nrow=3,byrow=T))+
-  theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1), legend.position = "none");p2
+  #guides(fill=guide_legend(nrow=3,byrow=T))+
+  theme(axis.text.x=element_text(angle=60, hjust=1),
+        text=element_text(size=18),
+        legend.position = "none");p2
 
 #Full MAF-filtered dataset - 8,384,961 SNPs
 
 all.snp.het <- read.table(file = "data/Full_SNPs_InbreedingStats.csv", header = T, sep = "\t") %>% glimpse()
 
 all.snp.het <- all.snp.het %>% 
-  mutate(He=(N_SITES-E.HOM.)/N_SITES, Ho=(N_SITES-O.HOM.)/N_SITES)
+  mutate(He=(N_SITES-E.HOM.)/N_SITES, Ho=(N_SITES-O.HOM.)/N_SITES, POP=gsub("Baie des Chal","Chaleurs",POP))
+
 
 p3 <- ggplot()+
   geom_boxplot(data=all.snp.het, aes(x=POP, y=F, fill=POP), colour="black", alpha=0.7)+
   theme_bw()+
-  theme(axis.text.x=element_text(angle=45, vjust=0.8, hjust=1), legend.position = "none",
+  theme(text=element_text(size=18), 
+        axis.text.x=element_text(angle=45, vjust=.9, hjust=1), 
+        legend.position = "none",
         axis.title.x =element_blank());p3
 
 p3/p1
@@ -49,28 +55,39 @@ p4 <- ggplot()+
   geom_boxplot(data=all.snp.het, aes(x=POP, y=He), colour="black")+
   theme_bw()+
   xlab(label ="")+
-  guides(fill=guide_legend(nrow=3,byrow=T))+
-  theme(axis.text.x=element_text(angle=45, vjust=1, hjust=1), legend.position = "none");p4
+  #guides(fill=guide_legend(nrow=3,byrow=T))+
+  theme(axis.text.x=element_text(angle=60, hjust=1), 
+        text=element_text(size=18),
+        legend.position = "none");p4
 
-p4/p2
+
 
 # Tajima's D --------------------------------------------------------------
 
 #Ran filtered all SNP dataset with Taj D in vcftools, 8,384,961 SNPs and 1145 inds
 
-taj.d <- read.table("/mnt/sdb/SnowCrab_LCWGS/allsnpsFiltered.Tajima.D", header = T) %>% glimpse()
+taj.d <- read.table("/mnt/sdb/SnowCrab_LCWGS/SummaryStats/allsnpsFiltered.Tajima.D", header = T) %>% glimpse()
 
 taj.d <- taj.d %>% filter(!TajimaD == "NaN") %>% sample_n(10000)
 
 taj.d$Position <- paste0(taj.d$CHROM,  sep="_", taj.d$BIN_START)
 
 
-p3 <- ggplot()+
-      geom_line(data=taj.d, aes(x=Position, y=TajimaD, group=1))+
+p5 <- ggplot()+
+      geom_line(data=taj.d, aes(x=Position, y=TajimaD, group=1),colour="dodgerblue2")+
       labs(x= "SNP Position",
            y="Tajima's D")+
-      theme_bw()+
-      theme(axis.text.x=element_blank())
+      theme(axis.text.x=element_blank(),
+            text=element_text(size=18),
+            axis.line = element_line(linewidth = 1));p5
 
-ggsave("figures/AllSNPs_TajimasD.png", plot=p3, device="png", width=10, height=8, dpi=300)
+ggsave("figures/AllSNPs_TajimasD.png", plot=p5, device="png", width=10, height=8, dpi=300)
       
+p5/p4/p2
+
+
+ggsave("figures/Combined_TajD_Heterozygosity.png",
+       plot = last_plot(),
+       device = "png",
+       width=12,
+       height=10)
